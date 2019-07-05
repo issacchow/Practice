74e16d51c3620298d2d720206b90fbb1faaab748
zhouweifeng <86508962@qq.com>
2019-07-05 10:17:28 +0800

    演示ClassLoader.getSystemResources方法作用。另外，SpringSPI(SpringFactoriesLoader类) 与 JAVA原生SPI(ServiceLoader类) 都使用了该方法获取资源

--- src/main/java/com/isc/Main.java
+++ src/main/java/com/isc/Main.java
@@ -1,5 +1,7 @@
 package com.isc;
 
+import org.springframework.boot.SpringApplication;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -7,8 +9,35 @@ import java.util.Enumeration;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Enumeration<URL> systemResources;
+        ClassLoader classLoader;
 
-        Enumeration<URL> systemResources = ClassLoader.getSystemResources("META-INF/MANIFEST.MF");
+
+
+        classLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(String.format("#### using SystemClassLoader:%s ####",classLoader.getClass().getName()));
+
+
+        systemResources = classLoader.getResources("META-INF/MANIFEST.MF");
+        while (systemResources.hasMoreElements()) {
+            URL url = systemResources.nextElement();
+            System.out.println(url.getPath());
+        }
+
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        classLoader = SpringApplication.class.getClassLoader();
+        System.out.println(String.format("#### using some class loader:%s ####",classLoader.getClass().getName()));
+
+        systemResources = classLoader.getResources("META-INF/MANIFEST.MF");
         while (systemResources.hasMoreElements()) {
             URL url = systemResources.nextElement();
             System.out.println(url.getPath());
