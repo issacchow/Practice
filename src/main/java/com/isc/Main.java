eb2be1f05e86309dd79b2c91316f9a78f1e63efb
zhouweifeng <86508962@qq.com>
2019-07-05 09:54:04 +0800

    演示ClassLoader.getSystemResources方法作用。另外，SpringSPI(SpringFactoriesLoader类) 与 JAVA原生SPI(ServiceLoader类) 都使用了该方法获取资源

--- src/main/java/com/isc/Main.java
+++ src/main/java/com/isc/Main.java
@@ -6,16 +6,14 @@ import java.util.Enumeration;
 
 public class Main {
 
-    public static void main( String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         Enumeration<URL> systemResources = ClassLoader.getSystemResources("META-INF/MANIFEST.MF");
-        while(systemResources.hasMoreElements()){
-             URL url = systemResources.nextElement();
+        while (systemResources.hasMoreElements()) {
+            URL url = systemResources.nextElement();
             System.out.println(url.getPath());
         }
     }
-
-
 
 
 }
