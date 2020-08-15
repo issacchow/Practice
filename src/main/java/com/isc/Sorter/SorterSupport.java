9f03b47486050333b40646e722bce4c0f30a3243
IssacChow <danny99899@gmail.com>
2020-08-16 07:34:08 +0800

    增加顺序检查方法

--- src/main/java/com/isc/Sorter/SorterSupport.java
+++ src/main/java/com/isc/Sorter/SorterSupport.java
@@ -27,10 +27,10 @@ public class SorterSupport {
     }
 
     void pln(){
-        System.out.println();
+//        System.out.println();
     }
     void pln(String content){
-        System.out.println(content);
+//        System.out.println(content);
     }
 
     int[] randomArray() {
@@ -40,5 +40,17 @@ public class SorterSupport {
             arr[i] = random.nextInt(100);
         }
         return arr;
+    }
+
+    /**
+     * 检查顺序是否有错误
+     * @param arr
+     */
+    void checkOrder(int[] arr) throws RuntimeException {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>arr[i+1]){
+                throw new RuntimeException("this is disorder");
+            }
+        }
     }
 }
