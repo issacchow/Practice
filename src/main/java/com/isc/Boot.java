f291e7496e763e46d43c608ddd42f04eddc013dc
zhouweifeng <86508962@qq.com>
2021-06-04 17:22:38 +0800

    修正mapper.xml没有生成的问题，调整日志配置

--- src/main/java/com/isc/Boot.java
+++ src/main/java/com/isc/Boot.java
@@ -1,25 +1,73 @@
 package com.isc;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.isc.dto.UserDto;
 import com.isc.mapper.UserMapper;
+import com.isc.request.UserSearchRequest;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication(scanBasePackageClasses = Boot.class)
 @RestController("/")
 public class Boot {
 
+
+    // 旧版
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+//        paginationInterceptor.(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(Boot.class);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Autowired
@@ -35,14 +83,23 @@ public class Boot {
     }
 
     @RequestMapping("update")
-    public UserDto updateUser(@RequestBody UserDto user) {
-        userMapper.updateById(user);
-        return user;
+    public int updateUser(@RequestBody UserDto user) {
+        return userMapper.updateById(user);
     }
 
     @RequestMapping("get/{userId}")
     public UserDto getUser(@PathVariable("userId") Integer userId){
         return userMapper.selectById(userId);
     }
+
+    @RequestMapping("search")
+    public List<UserDto> search(@RequestBody UserSearchRequest request){
+
+        Page<UserDto> pager = new Page<>(request.getPage(),10);
+        IPage<UserDto> result = userMapper.selectPageVo(pager);
+
+        return result.getRecords();
+    }
+
 
 }
--- src/main/java/com/isc/mapper/UserMapper.java
+++ src/main/java/com/isc/mapper/UserMapper.java
@@ -1,9 +1,13 @@
 package com.isc.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.isc.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper extends BaseMapper<UserDto> {
+
+    IPage<UserDto> selectPageVo(Page<?> page);
 }
