29fdbb8870926aed1603790ea344f88ec045d749
zhouweifeng <86508962@qq.com>
2021-06-09 10:33:18 +0800

    最简单的spring boot hello world

--- src/main/java/com/isc/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.isc.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.isc.dto.UserDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<UserDto> {
-}
