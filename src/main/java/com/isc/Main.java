1901fb7e7ca5d6f53aa13819a0ab6484761019a6
zhouweifeng <86508962@qq.com>
2021-12-29 20:48:15 +0800

    增加Jedis 测试代码，用于调试网络数据包，需要使用wireshark 来配合

--- src/main/java/com/isc/Main.java
+++ src/main/java/com/isc/Main.java
@@ -3,7 +3,7 @@ package com.isc;
 import redis.clients.jedis.Jedis;
 
 /**
- * 本示例主要测试一下，使用jedis 设置值，是否需要同步等待响应才返回结果(有可能是void结果)给调用方
+ * 本示例主要测试一下，通过抓包方式，使用jedis 设置值，是否需要同步等待响应才返回结果(有可能是void结果)给调用方
  * (需要使用wireshark 来配合)
  */
 public class Main {
@@ -20,6 +20,11 @@ public class Main {
         // 同时也证明了另外一样东西, socket发送数据包后，即使接收方没有发送实质数据，发送方(即socket)还是会收到一个非PSH 的ACK
         // 此时socket是没有收到数据，仅仅收到一个非PSH的ACK
         jedis.set("source-reading/jedis","hello!");
+
+
+        // 下面这句请断点调试，在flush()执行 后马上中断，即不收ACK,这样可以模拟一下网络抖动 或 程序中断，导致操作假失败
+        jedis.set("source-reading/jedis/ReturnWithoutACK","hello!");
+
 
         jedis.disconnect();
     }
