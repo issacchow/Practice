1a1f56c29702c6567e9c8f534b7400f0459c0803
IssacChow <danny99899@gmail.com>
2022-02-20 10:45:39 +0800

    断点查看classloader加载类的过程

--- src/main/java/com/isc/Main.java
+++ src/main/java/com/isc/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // 这里断点到classloader的 loadClass(String name, boolean resolve) 方法看看加载过程
+        // 断点时加上断点条件为:  name.equal("com.isc.SomeClz")
+        SomeClz clz = new SomeClz();
     }
 
 
---
+++ src/main/java/com/isc/SomeClz.java
@@ -0,0 +1,4 @@
+package com.isc;
+
+public class SomeClz {
+}
