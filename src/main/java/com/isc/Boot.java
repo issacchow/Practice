9a56828505bd81e1c83af839cf04b4354bbab2e7
zhouweifeng <86508962@qq.com>
2023-01-11 10:28:32 +0800

    【临时提交】quartz调度器

--- src/main/java/com/isc/Boot.java
+++ src/main/java/com/isc/Boot.java
@@ -1,13 +1,25 @@
 package com.isc;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackageClasses = Boot.class)
 @RestController("/")
 public class Boot {
+
+
+    @Bean
+    public Scheduler quartzScheduler() throws SchedulerException {
+
+        StdSchedulerFactory factory = new StdSchedulerFactory();
+        return factory.getScheduler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Boot.class);
@@ -19,6 +31,8 @@ public class Boot {
     public String helloWorld() {
         return "hello world";
     }
+
+
 
 
 }
