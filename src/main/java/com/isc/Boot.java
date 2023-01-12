5080e3185a57a3f8db1dae6b6c63256d050743a3
zhouweifeng <86508962@qq.com>
2023-01-12 21:50:13 +0800

    临时提交

--- src/main/java/com/isc/Boot.java
+++ src/main/java/com/isc/Boot.java
@@ -3,11 +3,15 @@ package com.isc;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication(scanBasePackageClasses = Boot.class)
 @RestController("/")
@@ -18,19 +22,35 @@ public class Boot {
     public Scheduler quartzScheduler() throws SchedulerException {
 
         StdSchedulerFactory factory = new StdSchedulerFactory();
-        return factory.getScheduler();
+        Scheduler scheduler = factory.getScheduler();
+        return scheduler;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Boot.class);
+
+
     }
 
 
+    @Autowired
+    Scheduler scheduler;
 
-    @RequestMapping("")
-    public String helloWorld() {
+    AtomicInteger jobNumber = new AtomicInteger(0);
+
+    @RequestMapping("addjob")
+    public String addjob(@RequestParam("delay") int delaySeconds) {
+
+
+//        JobDetail jobDetail = JobBuilder.newJob(QuartzSchedulingServiceJob.class)
+//                .withIdentity(, null)
+//                .build();
+//        JobDetailImpl job = new JobDetailImpl();
+//        scheduler.scheduleJob(n
         return "hello world";
     }
+
+
 
 
 
